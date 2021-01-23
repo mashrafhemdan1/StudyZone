6103cb19ad52844b95be998373c9980bd2f07902
ahmede8 <39431291+ahmede8@users.noreply.github.com>
2021-01-23 06:22:15 +0200

    Add files via upload

---
+++ iosUI/Proto/ProtoTests/ProtoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProtoTests.swift
+//  ProtoTests
+//
+//  Created by CSE on 1/22/21.
+//  Copyright © 2021 CSE. All rights reserved.
+//
+
+import XCTest
+@testable import Proto
+
+class ProtoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
